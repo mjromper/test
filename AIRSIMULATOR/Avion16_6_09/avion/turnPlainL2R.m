0c42b9e6a88995b2c0c91283f19034632d24cd6d
mjromper@gmail.com <mjromper@gmail.com@ac5a265e-ee2e-11dd-ada0-abde575de245>
2009-07-27 18:25:13 +0000

    Avion adaptado por Francisco. HAY QUE AJUSTAR LOS GIROS

---
+++ AIRSIMULATOR/Avion16_6_09/avion/turnPlainL2R.m
@@ -0,0 +1,21 @@
+function turnPlainL2R()
+
+
+global textura;
+global avion;
+global plano;
+pausa=0.02;
+texturaGiro = 0.002;
+avionGiro = 0.01;
+avanceRecto = 0.001;
+avanceGiro = 0.0008;
+%textura.center = [0.15 -0.5];
+
+    for i=0:20
+       %textura.rotation=textura.rotation+texturaGiro;
+        textura.translation = textura.translation + [avanceGiro 0];
+        avion.rotation = avion.rotation + [avionGiro 0 0 0];
+        vrdrawnow;
+        pause(pausa);
+       
+    end,
